97ac04daaea74de6ce13834130da010723fd7c0d
Zetrax <34830529+theZetrax@users.noreply.github.com>
2021-10-06 00:12:21 +0300

    Added AntDesign UI library

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'antd'
+import './App.css'
+
+const App = (props: any) => (
+  <div>
+    <h1>Hello, World!</h1>
+    <br />
+    <div style={{ padding: '3rem' }}>
+      <Button type="primary">Click Me</Button>
+    </div>
+  </div>
+)
+
+export default App
